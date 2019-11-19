703c39caed4b972d32ee5745562da81dec66b0f3
dongyoun <ehddbs6425@naver.com>
2019-11-19 17:57:28 +0000

    add move_tower_down() function

--- p1.c
+++ p1.c
@@ -14,25 +14,29 @@
 #define LEFTEDGE 20
 #define RIGHTEDGE 80
 #define TIMEVAL 40
+#define TOWERBOTTOM 20     //화면에서의 제일 아래 블럭 y축위치
+#define MAXVIEWEDBLOCKS 4  //게임중 화면에 보여질 블럭의 개수
 
 int dir = 1;
 int pos = LEFTEDGE;
-int FLOOR = 20;
+int FLOOR = TOWERBOTTOM;
 char blank[] = "        ";
-double arrCenterX[100];
+double arrCenterX[100];     //블럭의 무게중심 x좌표들의 배열(인덱스는몇번쨰 블럭인지)
+int arrBlockPosition[100];  //블럭의 왼쪽끝 x좌표들의 배열(인덱스는 몇번째 블럭인지)
 int numStackedBlocks = 0;
 
 void sig_handler();
 int set_ticker(int n_msecs);
 void set_cr_noecho_mode();
 void stack_tower();	// 탑이 밑으로 쌓이는 과정
-int can_stack(double);
-void view_stack_cnt();
+int can_stack(double);          //탑이 무너지지않게 블럭을 쌓을 수 있는지 체크하는 함수(쌓을 수 있으면 T, 없으면 F 반환)
+void view_stack_cnt();          
+void move_tower_down();         //화면에 일정 개수의 블럭이 쌓이면 탑을 아래로 내려줌 
 
 int main()
 {
 	char c;
-	char collapsed[] = "Tower is collapsed!!! You failed...";
+	char collapsed[] = "Tower is collapsed!!!!!!!!";
     char collapsed2[30];
 	initscr();
 	set_cr_noecho_mode();
@@ -66,10 +70,13 @@ int main()
                 endwin();
                 return 0;
 			}
+            arrBlockPosition[numStackedBlocks] = pos;
+            if(numStackedBlocks > MAXVIEWEDBLOCKS)
+                move_tower_down();
+            else
+			    FLOOR -= 1; // 한층이 쌓였으니깐, FLOOR -1을 시킨다.
 
-			FLOOR -= 1; // 한층이 쌓였으니깐, FLOOR -1을 시킨다.
 			pos = rand() % (RIGHTEDGE - LEFTEDGE) + LEFTEDGE;
-
 			set_ticker(TIMEVAL);
 			break;
 
@@ -78,6 +85,21 @@ int main()
 			return 0;
 		}
 	}
+}
+
+void move_tower_down(void)
+{
+    int row, idx;
+    
+    standend();
+    for(row = FLOOR, idx = numStackedBlocks; row <= TOWERBOTTOM; row++, idx--)
+        mvaddstr(row, arrBlockPosition[idx], blank);
+    
+    standout();
+    for(row = FLOOR+1, idx = numStackedBlocks; row <= TOWERBOTTOM; row++, idx--)
+        mvaddstr(row, arrBlockPosition[idx], blank);
+    
+    refresh();
 }
 
 int can_stack(double leftX)
@@ -104,6 +126,7 @@ int can_stack(double leftX)
 	arrCenterX[numStackedBlocks] = curCenterX;
 	return TRUE;
 }
+
 void sig_handler() // 블럭이 좌우로 움직이는 구간
 {
 	move(0, pos);
@@ -130,6 +153,7 @@ void view_stack_cnt() {
 	mvaddstr(30, RIGHTEDGE+10, stack_cnt_string);
 
 }
+
 int set_ticker(int n_msecs)
 {
 	struct itimerval new_timeset;
