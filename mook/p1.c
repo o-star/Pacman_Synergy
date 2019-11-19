c2ae996af06f88cad96ae2ca1a9278c4c1f288e4
mook <kcm0147@hanmail.net>
2019-11-19 22:23:07 +0900

    Add view_stack_cnt fucntion()
    게임 도중 몇개의 블럭이 쌓였는지 확인할 수 있게 함
    또한 블럭이 붕괴된 이후 발생하던 오류 수정
    (화면이 사라지면서 정상적으로 게임 종료)

---
+++ mook/p1.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <curses.h>
+#include <unistd.h>
+#include <signal.h>
+#include <termios.h>
+#include <sys/time.h>
+
+
+#define LEFTEDGE 30
+#define RIGHTEDGE 60
+#define TIMEVAL 40
+
+int dir = 1;
+int pos = LEFTEDGE;
+int FLOOR = 20;
+char blank[] = "     ";
+
+void sig_handler();
+int set_ticker(int n_msecs);
+void set_cr_noecho_mode();
+void stack_tower();	// 탑이 밑으로 쌓이는 과정
+void view_stack_cnt();
+
+
+int main()
+{
+	char c;
+	initscr();
+	set_cr_noecho_mode();
+	clear();
+	
+
+	signal(SIGALRM, sig_handler);
+	
+	
+	if(set_ticker(TIMEVAL) == -1)
+		perror("set_ticker");
+	
+	
+	while(1){
+		c = getchar();
+		switch(c){
+			case ' ':
+				set_ticker(2000); // 탑이 다 떨어질때 까지의 시간 멈춰두는것
+				stack_tower();
+				FLOOR -= 1;
+				set_ticker(TIMEVAL);
+				break;
+				sleep(2);
+                endwin();
+			case 'q':
+				endwin();
+				return 0;
+		}
+	}
+}
+
+
+void sig_handler()
+{	
+	move(0, pos);
+	standend();
+	addstr(blank);
+	pos += dir;
+	if(pos>=RIGHTEDGE)
+		dir = -1;
+	if(pos<=LEFTEDGE)
+		dir = +1;
+	move(0, pos);
+	standout();
+	addstr(blank);
+	move(LINES-1, COLS-1);
+	refresh();
+}
+
+void view_stack_cnt() {
+
+	char stack_cnt_string[100];
+	sprintf(stack_cnt_string, "Stacked block : %d ", numStackedBlocks);
+	mvaddstr(30, RIGHTEDGE+10, stack_cnt_string);
+
+}
+int set_ticker(int n_msecs)
+{
+	struct itimerval new_timeset;
+	long n_sec, n_usecs;
+
+	n_sec = n_msecs / 1000;
+	n_usecs = (n_msecs % 1000) * 1000L;
+
+	new_timeset.it_interval.tv_sec = n_sec;
+	new_timeset.it_interval.tv_usec = n_usecs;
+	new_timeset.it_value.tv_sec = n_sec;
+	new_timeset.it_value.tv_usec = n_usecs;
+
+	return setitimer(ITIMER_REAL, &new_timeset, NULL);
+}
+
+void set_cr_noecho_mode()
+{
+	struct termios ttystate;
+
+	tcgetattr(0, &ttystate);
+	ttystate.c_lflag &= ~ICANON;
+	ttystate.c_lflag &= ~ECHO;
+	ttystate.c_cc[VMIN] = 1;
+	tcsetattr(0, TCSANOW, &ttystate);
+}
+
+void stack_tower()
+{
+	int row_pos = 0;
+
+
+	while (1) {
+		move(row_pos, pos);
+		standend();
+
+		addstr(blank);
+
+		row_pos += 1;
+
+		move(row_pos, pos);
+		standout();
+		addstr(blank);
+		curs_set(0);
+		refresh();
+		usleep(50000);		// 1�� �̸� �����ٶ� ���
+
+		if (row_pos == FLOOR)
+			break;
+	}
+}
